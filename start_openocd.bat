b8982cbec7713010922e75ea632c95de20c36df6
CubeStayAlive <CubeStayAlive@gmx.net>
2018-03-27 11:13:37 +0200

    add batches

---
+++ start_openocd.bat
@@ -0,0 +1,2 @@
+start ..\..\openocd\bin-x64\openocd.exe --file ..\..\openocd\scripts\board\stm32f4discovery.cfg
+rem pause
---
+++ start_telnet_client.bat
@@ -0,0 +1,2 @@
+start ..\..\putty\putty64.exe telnet://localhost:4444
+rem pause
